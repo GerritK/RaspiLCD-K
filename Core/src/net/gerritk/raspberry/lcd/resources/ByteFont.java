ff4da8b3c554de5a5660e4cb7e9196424487c369
Gerrit Kaul <webmaster@gerritk.net>
2015-03-11 21:43:51 +0100

    added byte font support

---
+++ Core/src/net/gerritk/raspberry/lcd/resources/ByteFont.java
@@ -0,0 +1,55 @@
+package net.gerritk.raspberry.lcd.resources;
+
+import net.gerritk.raspberry.lcd.RaspiLCD;
+import net.gerritk.raspberry.lcd.interfaces.Font;
+import net.gerritk.raspberry.lcd.interfaces.Screen;
+
+public class ByteFont implements Font {
+	private net.gerritk.bytefont.ByteFont byteFont;
+	private RaspiLCD lcd;
+
+	public ByteFont(net.gerritk.bytefont.ByteFont byteFont, RaspiLCD lcd) {
+		this.byteFont = byteFont;
+		this.lcd = lcd;
+	}
+
+	@Override
+	public void drawString(String s, int x, int y) {
+		for(char c : s.toCharArray()) {
+			byte[][] bytes = byteFont.getBytes(c);
+			for(int xb = 0; xb < bytes.length; xb++) {
+				for(int yb = 0; yb < bytes[1].length; yb++) {
+					byte b = bytes[xb][yb];
+					for(int bit = 0; bit < 8; bit++) {
+						int dx = x + xb;
+						int dy = y + yb * 8 + bit;
+
+						if(dx < Screen.WIDTH && dy < Screen.HEIGHT && yb * 8 + bit < getHeight()) {
+							if (((b >> bit) & 1) == 1) {
+								lcd.getScreen().setPixel(dx, dy, lcd.getPenColor());
+							}
+						}
+					}
+				}
+			}
+			x += bytes.length;
+		}
+	}
+
+	@Override
+	public int getHeight() {
+		return byteFont.getHeight();
+	}
+
+	@Override
+	public int getWidth(String s) {
+		int width = 0;
+
+		for(char c : s.toCharArray()) {
+			byte[][] bytes = byteFont.getBytes(c);
+			width += bytes.length;
+		}
+
+		return width;
+	}
+}
--- Demo/src/net/gerritk/raspberry/lcd/demo/RaspiLCDDemo.java
+++ Demo/src/net/gerritk/raspberry/lcd/demo/RaspiLCDDemo.java
@@ -13,17 +13,20 @@
 
 package net.gerritk.raspberry.lcd.demo;
 
+import net.gerritk.bytefont.ByteFontReader;
 import net.gerritk.raspberry.lcd.HwScreen;
 import net.gerritk.raspberry.lcd.RaspiLCD;
 import net.gerritk.raspberry.lcd.input.ButtonEvent;
 import net.gerritk.raspberry.lcd.input.ButtonListener;
 import net.gerritk.raspberry.lcd.input.Buttons;
 import net.gerritk.raspberry.lcd.interfaces.Screen;
+import net.gerritk.raspberry.lcd.resources.ByteFont;
 import net.gerritk.raspberry.lcd.resources.Color;
 import net.gerritk.raspberry.lcd.simulator.RaspiLCDSimulator;
 import net.gerritk.raspberry.lcd.simulator.SimScreen;
 
 import java.awt.*;
+import java.io.File;
 
 public class RaspiLCDDemo implements Runnable, ButtonListener {
     private final RaspiLCD raspiLCD;
@@ -33,6 +36,7 @@ public class RaspiLCDDemo implements Runnable, ButtonListener {
 
     public RaspiLCDDemo(RaspiLCD raspiLCD) {
         this.raspiLCD = raspiLCD;
+		this.raspiLCD.setFont(new ByteFont(ByteFontReader.loadByteFont(new File("Demo/res/arial_p_8.bff")), this.raspiLCD));
 
         Buttons.addListener(this);
 
@@ -48,7 +52,19 @@ public class RaspiLCDDemo implements Runnable, ButtonListener {
             last = System.currentTimeMillis();
 
             raspiLCD.getScreen().clear();
-            raspiLCD.getScreen().setPixel(point.x, point.y, Color.BLACK);
+
+			raspiLCD.setFillColor(Color.BLACK);
+			raspiLCD.setPenColor(Color.WHITE);
+
+			for(int i = 0; i < 4; i++) {
+				raspiLCD.drawRect(Screen.WIDTH / 4 * i, Screen.HEIGHT - 12, Screen.WIDTH / 4 * (i + 1), Screen.HEIGHT);
+				raspiLCD.drawString(Screen.WIDTH / 4 * i + (Screen.WIDTH / 4 - raspiLCD.getFont().getWidth("TEST")) / 2, Screen.HEIGHT - (12 + raspiLCD.getFont().getHeight()) / 2, "TEST");
+			}
+
+			for(int i = 0; i < 2; i++) {
+				raspiLCD.drawRect(Screen.WIDTH - 12, (Screen.HEIGHT - 12) / 2 * i, Screen.WIDTH, (Screen.HEIGHT - 12) / 2 * (i + 1));
+			}
+
             raspiLCD.getScreen().flush();
 
             while(System.currentTimeMillis() - last < 250) {
